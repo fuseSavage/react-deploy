f8c3b7ae7fa4425dd9cae31fbd562abed54a865b
fuseSagave <chaiwat.si.61@ubu.ac.th>
2021-02-18 11:42:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src='https://scontent.fubp1-1.fna.fbcdn.net/v/t1.0-9/78838738_2593977317357224_1176424308679901184_o.jpg?_nc_cat=105&ccb=3&_nc_sid=e3f864&_nc_eui2=AeGuWlfYisQq_h0t9AfF0vJB9i9ubzAdRWL2L25vMB1FYnRsd53FG3M0nsphC9A55SAbGwquM1781K36ZqXyKXzL&_nc_ohc=WyPHzF4pme4AX_RogPM&_nc_ht=scontent.fubp1-1.fna&oh=e4a19a60ad9eb89456065e0052dbf601&oe=6051E0F1'
+          className="App-logo" alt="logo" />
+        <div>
+          <p>
+            <b>Name : </b>Chaiwat Singkibut
+          </p>
+          <p>
+            <b>Age : </b>23
+          </p>
+          <p>
+            <b>Contact : </b>58/3 Mahachanachai, Yasothon, 35130
+          </p>
+          <p>
+            <b>Phone : </b>093-3255417
+          </p>
+          <p>
+            <b>E-mail : </b>chaiwat.si.61@ubu.ac.th
+          </p>
+          <p>
+            <b>EDUCATON : </b> Studying a bachelor's degree in Computer Science at Ubon
+Ratchathani University GPAX 3.03
+
+          </p>
+        </div>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/profile.php?id=100002349439643"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          click to Profile
         </a>
       </header>
     </div>
